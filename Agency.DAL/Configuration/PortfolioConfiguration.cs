e1179ce7243dbc27fb3454dc32be5b977ea79708
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2024-01-15 08:37:54 +0400

    Add project files.

---
+++ Agency.DAL/Configuration/PortfolioConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Agency.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agency.DAL.Configuration
+{
+    public class PortfolioConfiguration : IEntityTypeConfiguration<Portfolio>
+    {
+        public void Configure(EntityTypeBuilder<Portfolio> builder)
+        {
+            builder.Property(c => c.Title).IsRequired().HasMaxLength(20);
+            builder.Property(c => c.Description).IsRequired().HasMaxLength(50);
+        }
+    }
+}
---
+++ Agency.DAL/Migrations/20240115043711_createdb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agency.DAL.Migrations
+{
+    public partial class createdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Portfolios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Portfolios");
+        }
+    }
+}
